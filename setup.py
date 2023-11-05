3fcdbef049589119e2daaac9c79a719c21882b65
Jeff Emanuel <35050222+Dicklesworthstone@users.noreply.github.com>
2023-11-04 20:52:02 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,28 @@
+import os
 from setuptools import setup
+
+# Define the directory where this setup.py file is located
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Read the contents of README file
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Read the contents of requirements file
+with open(os.path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    requirements = f.read().splitlines()
 
 setup(
     name='sqlalchemy_data_model_visualizer',
     version='0.1.0',  # Update the version number as needed
     description='A tool to visualize SQLAlchemy data models with Graphviz.',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     author='Jeffrey Emanuel',
     author_email='jeff@pastel.network',
     url='https://github.com/Dicklesworthstone/sqlalchemy_data_model_visualizer',  
     py_modules=['sqlalchemy_data_model_visualizer'],  # Single file module
-    install_requires=open('requirements.txt').read().splitlines(),
+    install_requires=requirements,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
